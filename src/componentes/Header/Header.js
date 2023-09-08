e74f964ea3fa0b8deb91f117b69a295e8230aaad
María José <majobonelli@gmail.com>
2023-09-08 15:07:35 -0300

    clase completa

---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css"
+
+function Header (){//los nombres siempre comienzan con mayus para diferenciar con las de html
+    return <header className="header">
+        <img src="/img/HeaderHeader_total.png" alt='Org'/>
+    </header>
+}
+
+export default Header
